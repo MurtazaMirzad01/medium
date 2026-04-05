a63be1dba03cb41bf8b1da8cd3d97554d69bbb94
MurtazaMirzad01 <murtazamirzad01@gmail.com>
2026-04-05 23:54:24 +0430

    Install breeze, create factory and seed the database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,11 +17,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Test user',
             'email' => 'test@example.com',
         ]);
+
+
+       $catagories = ['Technology', 'Health', 'Travel', 'Food', 'Education'];
+       foreach ($catagories as $category) {
+        Category::create([
+            'name' => $category,
+        ]);
+       }
+       Post::factory(100)->create();
     }
 }
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,39 @@
+<x-app-layout>
+
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+
+
+                    <ul class="flex flex-wrap text-sm font-medium text-center text-gray-500 dark:text-gray-400 justify-center">
+                        <li class="me-2">
+                            <a href="#" class="inline-block px-4 py-2.5 text-white bg-brand rounded-base active"
+                                aria-current="page">Tab 1</a>
+                        </li>
+                        <li class="me-2">
+                            <a href="#"
+                                class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">Tab
+                                2</a>
+                        </li>
+                        <li class="me-2">
+                            <a href="#"
+                                class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">Tab
+                                3</a>
+                        </li>
+                        <li class="me-2">
+                            <a href="#"
+                                class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">Tab
+                                4</a>
+                        </li>
+                        <li>
+                            <a class="inline-block px-4 py-3 text-fg-disabled cursor-not-allowed">Tab 5</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
